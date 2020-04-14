b24665d492e414d57e953e0c776672f732bf6bde
EDGART <edgar@getpoints.com>
2020-04-14 13:52:12 +0300

    removed local debugging code

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,20 @@
-const data = {
-    region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-}
+// const data = {
+//     region: {
+//     name: "Africa",
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// }
 
 // console.log(data)
 
-const covid19ImpactEstimator = () => {
+const covid19ImpactEstimator = (data) => {
 
     const impact = {...data};
     const severeImpact = {...data};
@@ -80,6 +80,6 @@ const covid19ImpactEstimator = () => {
     }
 };
 
-covid19ImpactEstimator(data);
+// covid19ImpactEstimator(data);
 
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
